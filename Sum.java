f494bce1d7a785fe3b3631288097cbb1890db816
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-05-27 12:27:56 +0200

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,7 @@
+public class Sum{
+    public static void main(String[] srgs){
+        int x = 25 ;
+        int y = 10 ;
+        System.out.println("X = " + x + "\nY = " + y + "\nX + Y = " + (x + y));
+    }
+}
